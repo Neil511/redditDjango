7a40889247280016dc5ce432980f93b8b2f21635
Neil Lohana <neil_lohana@hotmail.com>
2017-01-23 20:19:29 -0500

    Added first template for main page

--- reddit/main/views.py
+++ reddit/main/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there!");
+    context = { 'name': 'Name goes here',
+            'date': 'January 23rd, 2017'}
+    return render(request, 'index.html', context)
