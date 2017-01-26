916fcdcf99327005c5af07f07bddc82cd77b5f09
Neil Lohana <neil_lohana@hotmail.com>
2017-01-25 20:59:34 -0500

    Updating views to work with new model

--- reddit/main/views.py
+++ reddit/main/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 # Create your views here.
 def index(request):
     context = { 'name': 'Name goes here',
             'date': 'January 23rd, 2017'}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', posts)
+
+class Post:
+    def __init__(self, date, created_by, name, votes, comments, content):
+        self.name = name
+        self.date = date
+        self.created_by = created_by
+        self.votes = votes
+        self.comments = comments
+        self.content = content
+
+posts = [
+        Post(
+            datetime.now(),
+            "Neil",
+            "Post #1",
+            0,
+            None,
+            "Hello world!"),
+        Post(
+            datetime.now(),
+            "Neil",
+            "Post #2",
+            0,
+            None,
+            "Hello there!")]
