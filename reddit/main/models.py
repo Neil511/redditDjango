570952b6c5ad6414f31c5787fd7cfe0c904f4f56
Neil Lohana <neil_lohana@hotmail.com>
2017-01-27 00:01:50 -0500

    Added Post model, migrated and updated view to accept model

--- reddit/main/models.py
+++ reddit/main/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length = 200)
+    date = models.DateTimeField(auto_now_add = True)
+    created_by = models.CharField(max_length = 100)
+    votes = models.IntegerField()
+    comments = models.CharField(max_length = 100) #TODO: Update this to take a list of comments
+    content = models.TextField()
+
+    def __str__(self):
+        return self.name
--- reddit/main/views.py
+++ reddit/main/views.py
@@ -1,32 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+from .models import Post
 
-# Create your views here.
 def index(request):
+    posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
-
-class Post:
-    def __init__(self, date, created_by, name, votes, comments, content):
-        self.name = name
-        self.date = date
-        self.created_by = created_by
-        self.votes = votes
-        self.comments = comments
-        self.content = content
-
-posts = [
-        Post(
-            datetime.now(),
-            "Neil",
-            "Post #1",
-            0,
-            None,
-            "Hello world!"),
-        Post(
-            datetime.now(),
-            "Neil",
-            "Post #2",
-            0,
-            None,
-            "Hello there!")]
