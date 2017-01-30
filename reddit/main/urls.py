1fbf19c0fe29c1b3180ff534390b2c79bc5c7219
Neil Lohana <neil_lohana@hotmail.com>
2017-01-29 22:09:44 -0500

    Fixed url parameter capture

--- reddit/main/urls.py
+++ reddit/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
         url(r'^data/', views.data, name = 'data'),
         url(r'^posts/([0-9]+)/$', views.posts, name = 'posts'),
-        url(r'^user/(.)+', views.user, name = 'user'),
+        url(r'^user/(.+)', views.user, name = 'user'),
         url(r'^$', views.index, name = 'index'),
 ]
